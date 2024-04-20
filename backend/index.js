4bf61c5044451c0bc2325d1c1ff20c0287c679dd
YuvrajSharma92 <yuvrajsharma92@gmail.com>
2024-04-20 12:58:18 +0530

    apload project

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,11 @@ const connectToDB = async () => {
 connectToDB()
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: "*", 
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+  }))
 app.use(express.json())
 
 
